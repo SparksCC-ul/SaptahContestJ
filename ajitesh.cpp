48cb38d16626604dfd97f96275dda922ca4edf8b
ZZENGAA <69924289+ZZENGAA@users.noreply.github.com>
2021-01-25 22:07:29 +0530

    Update ajitesh.cpp

--- ajitesh.cpp
+++ ajitesh.cpp
@@ -1,17 +1,22 @@
 //name: ajitesh kumar soni
 //email: ajiteshhsonk@gmail.com
+//CSE-A
 #include<iostream>
 using namespace std;
 int main()
 {
-    int X[100],Y[100],sum[100],n;
+    int size;
+    int *X,*Y,*sum;
+    X = new int[size];
+    Y = new int[size];
+    sum = new int[size];
 
-    cout<<"enter the value of n :";
-    cin>>n;
-    cout<<"start entering values of X & Y upto "<<n<<" times"<<endl;
+    cout<<"enter size of array:";
+    cin>>size;
+    cout<<"start entering values of X & Y "<<endl;
 
 //loop for entering the values of x&y
-    for(int i=0;i<n;i++)
+    for(int i=0;i<size;i++)
         {
             cin>>X[i]>>Y[i];
             sum[i]=X[i]+Y[i];
@@ -19,9 +24,12 @@ int main()
     
     cout<<"the sum of all X & Y vaules"<<endl;
 //loop for displaying the sum of x&y
-    for(int i=0;i<n;i++)
+    for(int i=0;i<size;i++)
         {
             cout<<sum[i]<<endl;
         }
 
+delete [] X;
+delete [] Y;
+delete [] sum;
 }
