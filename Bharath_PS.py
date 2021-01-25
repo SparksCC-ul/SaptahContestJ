5706e47a3a345d6e70d1944dbba622e7d15349d6
Bharath P S <67433362+Bharathpedaballi@users.noreply.github.com>
2021-01-25 18:01:23 +0530

    Delete Bharath_PS.py

--- Bharath_PS.py
+++
@@ -1,6 +0,0 @@
-no_tests = int(input())
-for i in range(no_tests):
-    inputnumbers = input()
-    lstofnums = inputnumbers.split(" ")
-    total = int(lstofnums[0])+int(lstofnums[1])
-    print(total)
