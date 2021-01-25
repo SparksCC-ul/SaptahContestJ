c7907c1d0d3f9634dc4a447514d01eefeed4d41f
siddhantpriyadarshi18581 <77992570+siddhantpriyadarshi18581@users.noreply.github.com>
2021-01-26 02:22:27 +0530

    Sidhant.py

---
+++ Siddhant.py
@@ -0,0 +1,14 @@
+print('Tell me the first no.')
+x = int(input('>'))
+print('Tell me second no.')
+y = int(input('>'))
+
+def add():
+    f = x + y
+    print(f)
+
+
+add()
+
+
+# Email id - siddhantpriadarshi18581@gmail.com      Name - Siddhant Priyadarshi
