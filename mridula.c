a95bdbf317a6e26fd39ec7813bc30619cbd7a3d4
mridularamesh <77933720+mridularamesh@users.noreply.github.com>
2021-01-25 20:29:48 +0530

    Create mridula.c

---
+++ mridula.c
@@ -0,0 +1,11 @@
+/*NAME:MRIDULA.R ,email-mridularamesh3@gmail.com */
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("enter the value of num1 and num2\n");
+  scanf("%d %d",&num1,&num2);
+  sum = num1+num2;
+  printf("the sum of two numbers is %d",sum);
+  return 0;
+  }
