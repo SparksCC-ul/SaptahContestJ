9db35237ac6a8338841c1331ab7246b162ae1c36
Chinmayee10 <67498667+Chinmayee10@users.noreply.github.com>
2021-01-26 14:21:49 +0530

    Create Chinmayee2.c

---
+++ Chinmayee2.c
@@ -0,0 +1,41 @@
+//Nmae:R.CHINMAYEE 
+//E mail:chinmayeer10@gmail.com
+
+#include<stdio.h>
+  
+  void main()
+   {
+    int a,b,c;
+    
+    printf("   OUTPUT :\n");
+    printf("Enter any three no.s:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+
+
+	if(b>c)
+	printf("%d is the second largest no.",b);
+	else
+	printf("%d is the second largest no.",c);
+
+
+    if(b>a&&b>c)
+
+
+	if(a>c)
+	printf("the second largest no. is % d",a);
+	else
+	printf(" the second largest no. is %d",c);
+
+
+    if(c>a&&c>b)
+
+
+	if(b>a)
+	printf("second largest no. is %d",b);
+	else
+	printf("second largest no. is %d",a);
+
+
+    
+   }
