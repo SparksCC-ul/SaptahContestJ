7acb440c9c0d8b195df52c258dadc77370d8e6cb
AdarshSThambi <78009114+AdarshSThambi@users.noreply.github.com>
2021-01-26 12:24:35 +0530

    Create adarsh-day2.py

---
+++ adarsh-day2.py
@@ -0,0 +1,12 @@
+t = int(input())
+for i in range(0,t):
+    c=input().split()
+    for j in range(0,len(c)):
+        c[j]=int(c[j])
+    c.sort()
+    print(c[len(c)-2])
+    
+'''
+Adarsh S Thambi
+adarshsthambi@gmail.com 
+''' 
