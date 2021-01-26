32a9284e1778740748de2e7d969988b02847acaa
Subrat11Pandey <77990271+Subrat11Pandey@users.noreply.github.com>
2021-01-26 13:06:18 +0530

    Create Subrat.py

---
+++ Subrat.py
@@ -0,0 +1,38 @@
+#SUBRAT PANDEY
+#subratpandey100@gmail.com
+
+
+
+
+list = []
+
+T = int(input("Total number of trials :- "))
+if T >=1 and T<= 1000:
+
+    for b in range(T):
+        for i in range(3):
+            x = int(input("Enter the value :- " ))
+            if x >= 1 and x<= 1000000: 
+                list.append(x)
+            
+            
+            
+            else:
+                
+                print("out of range")
+                list=[]
+                break
+
+                
+                
+            
+            list.sort()
+        if len(list) == 0:
+            print("")
+        else:
+            print("Second highest digit is :- " , list[-2])
+            print("Next CASE")
+
+    print("End")
+else :
+    print("Enter the number between1 and 1000")
